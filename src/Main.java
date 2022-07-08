9e3c0d893b969cce9311d3a58d414e25b2dee82c
manuelrinconmendez <manuel.rinconmendez@outlook.com>
2022-07-07 21:44:49 -0500

    Subiendo segunda parte

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        coche miCoche = new coche();
+        miCoche.agregar_puertas();
+    }
+}
---
+++ src/coche.java
@@ -0,0 +1,8 @@
+public class coche {
+    public int puerta = 0;
+
+    public void agregar_puertas(){
+        this.puerta++;
+        System.out.println("El objeto tiene " + puerta + " puerta.");
+    }
+}
